5a76b07f722cbf40117a881d87c8b6391db66e50
Keenan Graham <keenangraham@stanford.edu>
2021-11-08 14:34:42 -0800

    Init

---
+++ tests/unit/test_fargate_flask_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from fargate_flask.fargate_flask_stack import FargateFlaskStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in fargate_flask/fargate_flask_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = FargateFlaskStack(app, "fargate-flask")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
