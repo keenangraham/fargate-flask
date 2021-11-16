90831c03ed849ac28ece52930627facdbb412136
Keenan Graham <keenangraham@stanford.edu>
2021-11-15 22:01:40 -0800

    Deploy GDS app

--- fargate_flask/flask/api.py
+++ fargate_flask/flask/api.py
@@ -1,33 +1,4 @@
-import requests
-
-from flask import Flask
-from flask import jsonify
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Success'
-
-
-@app.route('/health')
-def health():
-    return 'Yes'
-
-
-@app.route('/reverse/<string>')
-def reverse(string):
-    return string[::-1]
-
-
-@app.route('/es')
-def es():
-    r = requests.get(
-        'http://vpc-rna-expression-dro56qntagtgmls6suff2m7nza.us-west-2.es.amazonaws.com:80'
-    )
-    return jsonify(r.json())
+from genomic_data_service import app
 
 
 if __name__ == '__main__':
